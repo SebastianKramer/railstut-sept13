812b02bade2ba963150359205c8ff452f921e320
Sebastian Kramer <sebastiankramer@gmx.net>
2013-09-06 11:44:58 +0200

    change application.rb to get rid of heroku precompile problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailstutSept13Rails32::Application.config.secret_token = 'bb35138d9b10e58265dbf81eb8e6f8b94b7856efa59768a51b04038309ca60efc12878ee0725d8732dc5e4228190f463c7df388eacba7b97593ec4ee79353e43'
